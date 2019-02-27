a21b62d99d5fadf974b8ff3bf17dc4cb9d69c9fc
chrums <jake@againstgrav.com>
2019-02-27 11:24:11 -0800

    Working on synchronous dispatcher...

---
+++ Assets/Scripts/Common/DispatchableV2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class DispatchableV2 : MonoBehaviour
+{
+
+    [SerializeField]
+    private float m_Priority = 0.0f;
+    public float priority { get { return m_Priority; } protected set { m_Priority = value; } }
+
+    private DispatcherV2 m_Dispatcher;
+
+    void Start()
+    {
+        m_Dispatcher = GetComponent<DispatcherV2>();
+        m_Dispatcher.Register(m_Priority, this);
+    }
+
+    public virtual bool Query()
+    {
+        return false;
+    }
+
+    public virtual bool Run()
+    {
+        return true;
+    }
+
+    public virtual bool Interrupt(DispatchableV2 dispatchable)
+    {
+        return false;
+    }
+
+}
---
+++ Assets/Scripts/Common/DispatcherV2.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DispatcherV2 : MonoBehaviour
+{
+
+    private DispatchableV2 m_Active = null;
+    private SortedDictionary<float, List<DispatchableV2>> m_Dispatchables = new SortedDictionary<float, List<DispatchableV2>>();
+    
+    private void Update()
+    {
+
+        if (m_Active != null)
+            if (!m_Active.Run())
+                m_Active = null;
+
+        Dispatch();
+
+    }
+
+    public void Register(float priority, DispatchableV2 dispatchable)
+    {
+        List<DispatchableV2> dispatchables;
+        if (!m_Dispatchables.TryGetValue(priority, out dispatchables))
+        {
+            dispatchables = new List<DispatchableV2>();
+            m_Dispatchables[priority] = dispatchables;
+        }
+        dispatchables.Add(dispatchable);
+    }
+
+    private void Dispatch()
+    {
+        foreach (KeyValuePair<float, List<DispatchableV2>> priority in m_Dispatchables)
+            foreach (DispatchableV2 dispatchable in priority.Value)
+                if (dispatchable.Query())
+                {
+                    if (m_Active == null)
+                        m_Active = dispatchable;
+                    else if (m_Active.Interrupt(dispatchable))
+                        m_Active = dispatchable;
+                }
+    }
+
+}
