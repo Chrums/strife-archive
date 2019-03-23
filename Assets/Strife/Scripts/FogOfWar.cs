0f0f3149f0dec355e11ca16c7641eba1bd0cfb8d
Michael Schroder <thormme@yahoo.com>
2019-03-22 19:54:16 -0700

    Added more fogofwar stuff

---
+++ Assets/Strife/Scripts/FogOfWar.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FogOfWar : MonoBehaviour
+{
+
+    public GameObject fogOfWarPlane;
+    public Transform player;
+    public LayerMask fogLayer;
+    public float radius = 5f;
+    private float radiusSqr { get { return radius * radius; } }
+
+    private Mesh m_mesh;
+    private Vector3[] m_vertices;
+    private Color[] m_colors;
+
+    // Use this for initialization
+    void Start()
+    {
+        Initialize();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray r = new Ray(transform.position, player.position - transform.position);
+        RaycastHit hit;
+        if (Physics.Raycast(r, out hit, 1000, fogLayer, QueryTriggerInteraction.Collide))
+        {
+            for (int i = 0; i < m_vertices.Length; i++)
+            {
+                Vector3 v = fogOfWarPlane.transform.TransformPoint(m_vertices[i]);
+                float dist = Vector3.SqrMagnitude(v - hit.point);
+                if (dist < radiusSqr)
+                {
+                    float alpha = Mathf.Min(m_colors[i].a, dist / radiusSqr);
+                    m_colors[i].a = alpha;
+                }
+            }
+            UpdateColor();
+        }
+    }
+
+    void Initialize()
+    {
+        m_mesh = fogOfWarPlane.GetComponent<MeshFilter>().mesh;
+        m_vertices = m_mesh.vertices;
+        m_colors = new Color[m_vertices.Length];
+        for (int i = 0; i < m_colors.Length; i++)
+        {
+            m_colors[i] = Color.black;
+        }
+        UpdateColor();
+    }
+
+    void UpdateColor()
+    {
+        m_mesh.colors = m_colors;
+    }
+}
